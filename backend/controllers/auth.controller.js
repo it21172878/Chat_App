e47423dac3f469cc99b45dc2fece0a3d8a053edb
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2024-06-13 12:52:39 +0530

    Login Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -48,9 +48,30 @@ export const signUp = async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 };
-export const logIn = (req, res) => {
-  console.log('User login successfully!');
-  res.send('User login successfully!');
+
+// user login controller
+export const logIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ''
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: 'Invalid username or password' });
+    }
+    generateTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log('Error in login controller', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 export const logOut = (req, res) => {
   console.log('User logout successfully!');
